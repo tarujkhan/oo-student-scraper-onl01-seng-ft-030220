266ca75c0ca5145fc8a75bdba93b74c62c75451e
taroj khan <tarujkhan@yahoo.com>
2020-03-31 18:31:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,9 +15,9 @@ class Scraper
       profile_path = student.css("a").attribute("href").value
       student_details[:profile_url] = './fixtures/student-site/' + profile_path
       students << student_details
-       new_student = {student => "h4.student-name"}
-    new_location = {location => "p.student-location"}
-    new_profile = {profile =>  "./fixtures/student-site/"}
+       new_student = {:student => "h4.student-name"}
+    new_location = {:location => "p.student-location"}
+    new_profile = {:profile =>  "./fixtures/student-site/"}
     students << new_student 
     students << new_location
     students << new_profile
