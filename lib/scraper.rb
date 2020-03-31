355b441965d02389174d4a77255685d6682455d6
taroj khan <tarujkhan@yahoo.com>
2020-03-31 17:31:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ class Scraper
      students = []
     html = open(index_url)
     index = Nokogiri::HTML(html)
-    index.css("roster-cards-container").each do |student|
+    index.css("div.roster-cards-container").each do |student|
+      binding.pry
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
