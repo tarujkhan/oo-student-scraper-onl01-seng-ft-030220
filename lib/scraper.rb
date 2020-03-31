17cab762c8188fda90cc5215faff8501087aaee9
taroj khan <tarujkhan@yahoo.com>
2020-03-31 18:46:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,12 @@ class Scraper
 
   def self.scrape_index_page(index_url)
      students = []
+      student_details = {}
     html = open(index_url)
     index = Nokogiri::HTML(html)
     index.css("div.roster-cards-container").each do |student|
       #binding.pry
-      student_details = {}
+     
       student_details[:name] = student.css("h4.student-name").text[0]
       student_details[:location] = student.css("p.student-location").text[1]
       profile_path = student.css("a").attribute("href").value
