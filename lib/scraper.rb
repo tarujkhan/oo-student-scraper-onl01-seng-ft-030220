f45c79d93a0a10f40af6798f104d1cbfe7e39e66
taroj khan <tarujkhan@yahoo.com>
2020-03-31 17:55:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
     index.css("div.roster-cards-container").each do |student|
       #binding.pry
       student_details = []
-      student_details[:name] = student.css("h4.student-name").text
+      student_details[:name].to_i = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
       profile_path = student.css("a").attribute("href").value
       student_details[:profile_url] = './fixtures/student-site/' + profile_path
